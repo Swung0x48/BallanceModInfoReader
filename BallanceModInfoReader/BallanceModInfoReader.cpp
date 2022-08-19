9e6ae622f16354df1f0242baedbf37f9b9a7f50b
Swung0x48 <swung0x48@outlook.com>
2022-08-19 20:09:25 +0800

    feat: use PascalCase instead of camelCase

--- BallanceModInfoReader/BallanceModInfoReader.cpp
+++ BallanceModInfoReader/BallanceModInfoReader.cpp
@@ -13,12 +13,12 @@ int main(int argc, char** argv)
 	ModInfo info;
 	picojson::value v;
 	v.set<picojson::object>(picojson::object());
-	v.get<picojson::object>()["status"] = picojson::value(-1.0);
-	v.get<picojson::object>()["message"] = picojson::value("Internal Error");
+	v.get<picojson::object>()["Status"] = picojson::value(-1.0);
+	v.get<picojson::object>()["Message"] = picojson::value("Internal Error");
 	
 	if (argc != 2) {
-		v.get<picojson::object>()["status"] = picojson::value((double)ERROR_FILE_NOT_FOUND);
-		v.get<picojson::object>()["message"] = picojson::value("Usage: BallanceModInfoReader.exe <path to .bmod>");
+		v.get<picojson::object>()["Status"] = picojson::value((double)ERROR_FILE_NOT_FOUND);
+		v.get<picojson::object>()["Message"] = picojson::value("Usage: BallanceModInfoReader.exe <path to .bmod>");
 		std::cout << v.serialize();
 		return 0;
 	}
@@ -26,8 +26,8 @@ int main(int argc, char** argv)
 	//std::cout << argv[1] << std::endl;
 	HMODULE handle = LoadLibrary("BML.dll");
 	if (!handle) {
-		v.get<picojson::object>()["status"] = picojson::value((double)GetLastError());
-		v.get<picojson::object>()["message"] = picojson::value("BML stub load failed.");
+		v.get<picojson::object>()["Status"] = picojson::value((double)GetLastError());
+		v.get<picojson::object>()["Message"] = picojson::value("BML stub load failed.");
 		std::cout << v.serialize();
 		return 0;
 	}
@@ -35,27 +35,24 @@ int main(int argc, char** argv)
 	InfoFunc get_info = reinterpret_cast<InfoFunc>(GetProcAddress(handle, "GetModInfo"));
 	ModInfo* mod_instance = get_info(argv[1]);
 	if (!mod_instance) {
-		v.get<picojson::object>()["status"] = picojson::value((double)GetLastError());
-		v.get<picojson::object>()["message"] = picojson::value("Bmod load failed.");
+		v.get<picojson::object>()["Status"] = picojson::value((double)GetLastError());
+		v.get<picojson::object>()["Message"] = picojson::value("Bmod load failed.");
 		std::cout << v.serialize();
 		return 0;
 	}
-	picojson::value& mod = v.get<picojson::object>()["mod"];
+	picojson::value& mod = v.get<picojson::object>()["Mod"];
 	mod.set<picojson::object>(picojson::object());
-	mod.get<picojson::object>()["id"] = picojson::value(mod_instance->ID);
-	mod.get<picojson::object>()["name"] = picojson::value(mod_instance->Name);
-	mod.get<picojson::object>()["version"] = picojson::value(mod_instance->Version);
-	mod.get<picojson::object>()["description"] = picojson::value(mod_instance->Description);
-	mod.get<picojson::object>()["author"] = picojson::value(mod_instance->Author);
-	picojson::value& bml = mod.get<picojson::object>()["bml"];
+	mod.get<picojson::object>()["Id"] = picojson::value(mod_instance->ID);
+	mod.get<picojson::object>()["Name"] = picojson::value(mod_instance->Name);
+	mod.get<picojson::object>()["Version"] = picojson::value(mod_instance->Version);
+	mod.get<picojson::object>()["Description"] = picojson::value(mod_instance->Description);
+	mod.get<picojson::object>()["Author"] = picojson::value(mod_instance->Author);
+	picojson::value& bml = mod.get<picojson::object>()["Bml"];
 	bml.set<picojson::object>(picojson::object());
-	bml.get<picojson::object>()["major"] = picojson::value((double)mod_instance->BMLMajor);
-	bml.get<picojson::object>()["minor"] = picojson::value((double)mod_instance->BMLMinor);
-	bml.get<picojson::object>()["build"] = picojson::value((double)mod_instance->BMLBuild);
-	v.get<picojson::object>()["status"] = picojson::value(0.0);
-	v.get<picojson::object>()["message"] = picojson::value("OK");
-	//picojson::value& mod = v.get<picojson::object>()["mod"];
-	//mod.set<picojson::object>(picojson::object());
-	//mod.get<picojson::object>()["test"] = picojson::value("a");
+	bml.get<picojson::object>()["Major"] = picojson::value((double)mod_instance->BMLMajor);
+	bml.get<picojson::object>()["Minor"] = picojson::value((double)mod_instance->BMLMinor);
+	bml.get<picojson::object>()["Build"] = picojson::value((double)mod_instance->BMLBuild);
+	v.get<picojson::object>()["Status"] = picojson::value(0.0);
+	v.get<picojson::object>()["Message"] = picojson::value("OK");
 	std::cout << v.serialize();
 }
